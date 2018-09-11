c33da46bb2261cee80a45ee67644c1308caf37d8
苏震 <suzhen@viewhigh.com>
2018-09-11 23:03:35 +0800

    index page
    index page

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -8,8 +8,8 @@ function IndexPage() {
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+        {/* <li>To get started, edit <code>src/index.js</code> and save to reload.</li> */}
+        <li><a href="#/login">Login in</a></li>
       </ul>
     </div>
   );
